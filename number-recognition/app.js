067875f8265ff050bea8d7da2ae01a2004f9e740
Chase Haddleton <chase@chasehaddleton.com>
2016-09-17 15:30:14 -0400

    Added number-recognition code

---
+++ number-recognition/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var gcloud = require('gcloud')({
+    projectId: process.env.GCP_PROJECT,
+});
+
+var vision = gcloud.vision();
+var bodyParser = require('body-parser');
+
+//Make public folder public
+app.use(express.static('public'));
+app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/', function(req, res){
+    vision.detectText(req.body.image, {}, function(err, text) {
+        if (err) {
+            res.send("Error, vision failed.");
+        } else {
+            res.send({
+                text: text.desc
+            });
+        }
+    });
+});
+
+app.listen('3000', function(){
+    console.log('App running on port 3000');
+});
