813a5cccfc921dfefa594961420bb5ea6e89c885
Alex <alexwileyy@outlook.com>
2016-09-17 11:07:41 -0400

    Added confiuration for cloud storage

--- website/app.js
+++ website/app.js
@@ -1,3 +1,14 @@
 var express = require('express');
+var app = express();
 
-console.log('hello world');
+//Make public folder public
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+   res.send('something');
+});
+
+var server = app.listen(process.env.PORT || '8080', function () {
+   console.log('App listening on port %s', server.address().port);
+   console.log('Press Ctrl+C to quit.');
+});
