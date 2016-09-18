567d992e97facaccc321d2f654138e660c7ef5a8
Alex <alexwileyy@outlook.com>
2016-09-18 06:26:29 -0400

    kill me

--- website/app.js
+++ website/app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-var appRootDir = require('app-root-dir').get();
 var path = require('path');
+var request = require('request');
+var User = require('./connections');
 
 //Make public folder public
 app.use(express.static('public'));
@@ -14,8 +15,29 @@ app.get('/', function(req, res){
    res.render('index');
 });
 
-app.get('/nearby-locations', function(res, req){
+app.post('/nearby-locations', function(req, res){
 
+    var userObject = User.returnUser('New', function(err, person){
+        console.log(person);
+        res.send(person);
+    });
+
+    var options = {
+        method: 'post',
+        body:{
+            location:{
+                lat: req.body.lat,
+                lon: req.body.lon
+            },
+            keywords: req.body.keywords
+        },
+        json: true,
+        url: 'placeholder'
+    };
+
+    request(options, function(err, res, body){
+       console.log(body)
+    });
 });
 
 
