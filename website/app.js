1bcdd4ed82087e12d99a084c8d3035f5dfa7fbfb
Alex <alexwileyy@outlook.com>
2016-09-18 04:52:14 -0400

    Added shit at this point

--- website/app.js
+++ website/app.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var app = express();
+var appRootDir = require('app-root-dir').get();
+var path = require('path');
 
 //Make public folder public
 app.use(express.static('public'));
 
+//jade step
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res){
-   res.send('This is a holding page for travlr.');
+   res.render('index');
 });
 
-// app.use('/login', require('./routes/login.js'));
+app.get('/nearby-locations', function(res, req){
+
+});
+
 
 var server = app.listen(process.env.PORT || '8080', function () {
    console.log('App listening on port %s', server.address().port);
